1c64f5a612350a8acb65be169e5a6d9fe369d34b
Nomena Rabenasolo <ag1549@student.jamk.fi>
2025-12-04 09:46:24 +0200

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,9 @@
 import { useSession } from 'next-auth/react'
 import Login from "./components/Login"
 import Main from './components/Main'
-import { Spin } from 'antd'
 
 export default function Home() {
   const { data: session, status } = useSession()
-
-  if (status === 'loading') {
-    return (
-      <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#121212',
-      }}>
-        <Spin size="large" />
-      </div>
-    )
-  }
-
   if (status !== "authenticated") return <Login />
   return <Main />
 }
