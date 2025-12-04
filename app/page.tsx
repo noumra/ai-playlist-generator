a1630aa876aa6c57320fa4ebb496529108d80fe6
Nomena Rabenasolo <ag1549@student.jamk.fi>
2025-12-04 02:02:14 +0200

    deployment error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
+import dynamic from 'next/dynamic'
 import Login from "./components/Login"
-import Main from './components/Main'
 import { Spin } from 'antd'
+
+const Main = dynamic(() => import('./components/Main'), {
+  ssr: false,
+  loading: () => (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#121212',
+    }}>
+      <Spin size="large" />
+    </div>
+  )
+})
 
 export default function Home() {
   const { data: session, status } = useSession()
